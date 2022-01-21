73e2431ca2dc6682beb199c2f499d8fd2f48dadc
Abhishek Chauhan <76480451+abhishekchauhan15@users.noreply.github.com>
2022-01-21 15:27:30 +0530

    user connection done

--- public/script.js
+++ public/script.js
@@ -1,6 +1,13 @@
+const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3000",
+});
 
 let myVideoStream;
 
@@ -11,10 +18,43 @@ navigator.mediaDevices
     audio: true,
   })
   .then((stream) => {
-      myVideoStream = stream;
+    myVideoStream = stream;
     //   console.log(myVideoStream);
     addVideoStream(myVideo, stream);
+    // answering the call
+    peer.on("call", (call) => {
+      //when user call us we answer it 
+      call.answer(stream);
+      const video = document.createElement("video");
+
+      // add video stream to the call
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connecToNewUser(userId, stream);
+    });
   });
+
+// stream is basically user video
+
+// join this room
+peer.on("open", (id) => {
+  console.log(id);
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const connecToNewUser = (userId, stream) => {
+  console.log(`new user connected from ${userId}`);
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  //sending new user our own stream 
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
@@ -24,3 +64,35 @@ const addVideoStream = (video, stream) => {
 
   videoGrid.append(video);
 };
+
+
+
+
+// let text = document.querySelector('input');
+let text= $('input')
+
+$('html').keydown((e)=> {
+  if (e.which == 13 && text.val().length !== 0) {
+    console.log(text.val());
+    socket.emit('message', text.val());
+    text.val(' ');
+  }
+})
+
+
+socket.on('createMessage', message => {
+  console.log("this is commign form the server", message);
+})
+
+
+
+//socket.emit --sends the data
+//scoket.on --listens to the data
+// socket io --reatime engien
+// with norman server you gives request and it responds to it but in socket io you send request and server can request back for another thing
+// in socket io server does not have to wait for request from client to respond
+// socket io is a realtime engine
+// it creates a channel between client and server it like a tube ans msgs and trafic goes through that
+
+//peerjs - to send stream between 2 users and it is a realtime engine
+//web rtc- allows you to communicate in real time in web browser
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const { v4: uuidv4 } = require("uuid");
-app.use(express.static("public"));  
+
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+})
+
+app.use('/peerjs', peerServer);
+
+
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
@@ -12,6 +22,24 @@ app.get("/", (req, res) => {
 
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
+});
+
+// when socket io is connected to server it will listen to connection event
+io.on("connection", (socket) => {
+  //user will join the rooom
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    console.log(`${roomId} joined the room`);
+    //adding another user to the room
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+
+    //reciving the msg for the user
+    socket.on('message', message => {
+      //sending it to frontend in the same roomId
+      io.to(roomId).emit('createMessage', message);
+      
+    })
+  });
 });
 
 server.listen(3000, () => {
